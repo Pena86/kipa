34df0cfadb46ba9f4d6f09db5130262fece44220
Eero Penttilä <eero.penttila@student.oulu.fi>
2019-05-02 15:36:57 +0300

    sarjan tulossivun templaten muokkausta

--- web/tulosnaytto/urls.py
+++ web/tulosnaytto/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'tulosnaytto'
 urlpatterns = [
         url(r'^(?P<kisa_nimi>[^/]+)/tulokset/(?P<sarja_idt>.*)$', views.tulokset, name='tulokset'),
         #url(r'^kayttaja/(?P<user_name>[^/]+)/$', views.kayttajan_tiedot, name='kayttaja'),
